42cb38f9106360eb396fdedf5a370256d8ae5c2d
Lecko <102591517+Lecko08@users.noreply.github.com>
2024-05-24 00:57:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+   // console.log(`burgir`)
+
+   // window.alert(`you are heecked`)
+
+   // document.getElementById("idd").textContent = `sup ni`;
+
+   // let nobita = 0
+   // let tejas = "niga"
+
+   // console.log(tejas)
+   // console.log(nobita)
+   // console.log(`you got ${nobita} marks`)
+   // console.log(`you are a ${tejas}`)
+
+   // console.log(typeof tejas)
+   // console.log(typeof nobita)
+
+   // let age = 988
+   // let name = "halwa"
+   // let baccha = true
+
+   // document.getElementById("ke").textContent = `tum ${age} saal ke ho`
+   // document.getElementById("he").textContent = `tumhara naam ${name} hai`
+   // document.getElementById("je").textContent = `tum baccha ho? ${baccha}`
+
+   // let doracakes = 10
+
+   // doracakes = doracakes + 1 or doracakes += 1
+   // doracakes = doracakes - 1 or doracakes -= 1
+   // doracakes = doracakes / 2 or doracakes /= 1
+   // doracakes = doracakes * 2 or doracakes *= 1
+   // doracakes = doracakes ** 2 or doracakes **= 1
+   // let extraDoracakes = doracakes % 3 or doracakes %= (% = remainder) 
+
+   // doracakes++  (increment by 1)
+   // doracakes--  (decreased by 1)
+   // console.log(doracakes)
+   // console.log(extraDoracakes)
+
+   // let username;
+
+   // document.getElementById("mySubmit").onclick = function(){
+   //    username = document.getElementById("myText").value
+   //    console.log(username)
+   // }
+
+   // document.getElementById("mySubmit").onclick = function(){
+   //   username = document.getElementById("myText").value
+   //   document.getElementById("kke").textContent = `Sup ${username}`
+   // }
+
+   // type conversion (number <> booleans <> strings)
+
+   let age = window.prompt("how much brightness")
+   age = Number(age)
+   age+=1
+
+   console.log(age)
